55b3b277850a9e2fda75e97e1b9ef1cc68d90db6
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-12 00:26:07 +0530

    Allow opening video in new tab
    Closes #17

--- site/components/video-list.js
+++ site/components/video-list.js
@@ -2,12 +2,16 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export function Video({ hit: video }) {
+  const heading = (
+    <h4>
+      {video.uploader}: {video.track || video.title} by {video.artists.join(", ")}
+    </h4>
+  );
+
   return (
     <div key={video.id}>
       <Link href={`/video/${video.id}`}>
-        <h4>
-          {video.uploader}: {video.track || video.title} by {video.artists}
-        </h4>
+        <a>{heading}</a>
       </Link>
       <img className={styles.thumbnail} src={`https://i.ytimg.com/vi/${video.id}/hqdefault.jpg`} />
     </div>
