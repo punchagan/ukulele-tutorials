c810b515ec109406c7521585556b519a34cb2d01
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-15 00:22:11 +0530

    Switch function to an arrow function

--- site/lib/pages.js
+++ site/lib/pages.js
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
 
-export async function getAllVideos() {
+export const getAllVideos = async () => {
   const includeUnpublished = process.env.NODE_ENV !== "production";
 
   const publishedJson = path.join(process.cwd(), "..", "data", "published.json");
@@ -11,4 +11,4 @@ export async function getAllVideos() {
     data = data.filter(x => x.publish === 1);
   }
   return data;
-}
+};
