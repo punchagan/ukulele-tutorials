aab8af3669def15d3c9baeec6461106208ded4b9
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-16 21:30:15 +0530

    Extract postData from markIgnored function

--- site/lib/api.js
+++ site/lib/api.js
@@ -1,11 +1,11 @@
-export const markIgnored = videoId => {
+export const postData = (videoId, data) => {
   fetch(`/api/video/${videoId}`, {
     method: "PUT",
-    body: JSON.stringify({ ignore: 1 }),
-    headers: {
-      "Content-Type": "application/json"
-    }
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" }
   })
     .then(response => response.json())
     .then(data => console.log(data));
 };
+
+export const markIgnored = videoId => postData(videoId, { ignore: 1 });
