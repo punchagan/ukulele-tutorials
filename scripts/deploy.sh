1c36815bcf91485dfd6fa5f9e7b669a3eb23ba72
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-25 23:22:43 +0530

    Clean up deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,21 +2,11 @@
 set -e
 
 EXPORT_DIR="out"
-GIT_URL=$(git remote get-url origin)
 
 # Build the site
 pushd $(dirname $0)/../site/
 yarn build
 yarn export
-
-# Push to GitHub
-# pushd "${EXPORT_DIR}"
-# git init
-# git add .
-# git commit -m "Deploy to GitHub Pages" \
-#     --author "Puneeth Chaganti <punchagan@muse-amuse.in>"
-# git push --force "${GIT_URL}" master:gh-pages
-# popd
 
 # Push to muse-amuse.in
 rsync -azP --delete "${EXPORT_DIR}/" muse-amuse.in:~/ukulele.muse-amuse.in
