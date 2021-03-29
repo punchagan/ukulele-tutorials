e80c623ea8f08b1f3b75c06513a2cc15303e7886
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-30 02:32:50 +0530

    Add hotkeys for setting loop start/end timings

--- site/components/player.js
+++ site/components/player.js
@@ -1,16 +1,19 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 import styles from "../styles/Video.module.css";
-import { Button, Descriptions, Slider, Switch } from "antd";
+import { Button, Descriptions, Modal, Slider, Switch } from "antd";
+import { useHotkeys } from "react-hotkeys-hook";
 
 export default function Player({ url, start, end, onChange }) {
   const player = useRef(null);
+  const slider = useRef(null);
   const [loop, setLoop] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [playing, setPlaying] = useState(false);
   const [useLoop, setUseLoop] = useState(true);
   const [showLoopInfo, setShowLoopInfo] = useState(false);
   const [duration, setDuration] = useState(100);
+  const [showHelp, setShowHelp] = useState(false);
 
   const sliderChange = r => {
     const [rStart, rEnd] = r;
@@ -51,6 +54,37 @@ export default function Player({ url, start, end, onChange }) {
 
   const formatSeconds = value =>
     `${Math.floor(value / 60)}m ${Math.floor((value % 60) * 100) / 100}s`;
+
+  const decStart = start => start => sliderChange([Math.max(0, start - 0.1), end]);
+
+  // Hot-keys
+  useHotkeys("i", () => {
+    const [_, E] = slider.current.props.value;
+    const s = player.current.getCurrentTime();
+    sliderChange([s, E]);
+  });
+  useHotkeys("o", () => {
+    const [S, _] = slider.current.props.value;
+    const e = player.current.getCurrentTime();
+    sliderChange([S, e]);
+  });
+  useHotkeys(",", () => {
+    const [s, e] = slider.current.props.value;
+    sliderChange([Math.max(0, s - 0.1), e]);
+  });
+  useHotkeys(".", () => {
+    const [s, e] = slider.current.props.value;
+    sliderChange([Math.max(0, s + 0.1), e]);
+  });
+  useHotkeys("shift+,", () => {
+    const [s, e] = slider.current.props.value;
+    sliderChange([s, Math.min(duration, e - 0.1)]);
+  });
+  useHotkeys("shift+.", () => {
+    const [s, e] = slider.current.props.value;
+    sliderChange([s, Math.min(duration, e + 0.1)]);
+  });
+  useHotkeys("shift+/", () => setShowHelp(!showHelp));
 
   return (
     <div>
@@ -94,6 +128,7 @@ export default function Player({ url, start, end, onChange }) {
           <Descriptions.Item label="Played Seconds">{formatSeconds(currentTime)}</Descriptions.Item>
         </Descriptions>
         <Slider
+          ref={slider}
           value={[start, end]}
           max={duration}
           step={0.1}
@@ -103,6 +138,31 @@ export default function Player({ url, start, end, onChange }) {
           tooltipVisible={showLoopInfo}
           onChange={sliderChange}
         />
+
+        <Modal
+          title="Keyboard Shortcuts"
+          visible={showHelp}
+          footer={null}
+          onCancel={() => setShowHelp(!showHelp)}
+        >
+          <Descriptions column={1}>
+            <Descriptions.Item label={<pre>i</pre>}>
+              Set loop start to current playing time
+            </Descriptions.Item>
+            <Descriptions.Item label={<pre>o</pre>}>
+              Set loop end to current playing time
+            </Descriptions.Item>
+            <Descriptions.Item label={<pre>,</pre>}>Decrement loop start by 0.1s</Descriptions.Item>
+            <Descriptions.Item label={<pre>.</pre>}>Increment loop start by 0.1s</Descriptions.Item>
+            <Descriptions.Item label={<pre>&lt;</pre>}>
+              Decrement loop end by 0.1s
+            </Descriptions.Item>
+            <Descriptions.Item label={<pre>&gt;</pre>}>
+              Increment loop end by 0.1s
+            </Descriptions.Item>
+            <Descriptions.Item label={<pre>?</pre>}>Show this help</Descriptions.Item>
+          </Descriptions>
+        </Modal>
       </div>
     </div>
   );
