21c3e4fae5ffda9d3eea8440ba0a000db548253a
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-30 23:55:25 +0530

    Add script to check if all metadata is being saved correctly

---
+++ scripts/check-data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import json
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+DATA_DIR = os.path.join(os.path.dirname(HERE), 'data')
+
+
+def check_missing_metadata():
+    with open(os.path.join(DATA_DIR, 'published.json')) as f:
+        data = json.load(f)
+
+    published = [entry for entry in data if entry['publish'] == 1]
+    for entry in published:
+        if len(entry['chords']) == 0 or len(entry['artists']) == 0 or len(entry['composers']) == 0:
+            if "rabindra" in entry['album'].lower():
+                continue
+            print(f"{entry['track']} is broken ({entry['id']})")
+
+
+if __name__ == "__main__":
+    check_missing_metadata()
