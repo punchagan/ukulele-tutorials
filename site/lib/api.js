b11cf1d98792a4e3eb24ff78b15fb8586f088a01
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-03-31 14:11:22 +0530

    Make the end-point for updating video data synchronous

--- site/lib/api.js
+++ site/lib/api.js
@@ -5,6 +5,16 @@ export const getVideoMetadata = ({ id, channel }) => {
 };
 
 export const postData = (videoId, data) => {
+  const { chords, artists, composers } = data;
+  if (chords || artists || composers) {
+    data = {
+      ...data,
+      chords: chords.join(", "),
+      artists: artists.join(", "),
+      composers: composers.join(", ")
+    };
+  }
+
   return fetch(`/api/video/${videoId}`, {
     method: "PUT",
     body: JSON.stringify(data),
--- site/pages/api/video/[id].js
+++ site/pages/api/video/[id].js
@@ -1,36 +1,23 @@
 import fs from "fs";
-import * as csv from "fast-csv";
+import * as csv from "csv/lib/sync";
 import path from "path";
 import { execFileSync } from "child_process";
 
 const editCSVRow = (videoId, data) => {
   const csvFile = path.join(process.cwd(), "..", "data", "tutorials.csv");
-  const outFile = path.join(process.cwd(), "..", "data", "tutorials-2.csv");
   const script = path.join(process.cwd(), "..", "scripts", "update-data.py");
-  const parse = csv.parse({
-    headers: true
-  });
-  const writeCSV = fs.createWriteStream(outFile);
-  const transform = csv.format({ headers: true }).transform(row => {
+
+  const csvData = fs.readFileSync(csvFile, { encoding: "utf-8" });
+  const parsedData = csv.parse(csvData, { columns: true });
+  const updatedData = csv.transform(parsedData, row => {
     if (row.id == videoId) {
       return { ...row, ...data };
     }
     return row;
   });
-  const stream = fs
-    .createReadStream(csvFile)
-    .pipe(parse)
-    .pipe(transform)
-    .pipe(writeCSV);
-
-  fs.rename(outFile, csvFile, err => {
-    if (err) throw err;
-    console.log(`Updated ${csvFile}!`);
-    setTimeout(() => {
-      execFileSync(script);
-      console.log("Updated published.json");
-    }, 2000);
-  });
+  fs.writeFileSync(csvFile, csv.stringify(updatedData, { header: true }), { encoding: "utf-8" });
+  execFileSync(script);
+  console.log("Updated published.json");
 };
 
 export default function handler(req, res) {
