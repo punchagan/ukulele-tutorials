3a4d60e6cfa375026065c1c4fa42ec550d96bf24
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-04-23 16:25:45 +0530

    Fix typos in formatting string in check-data script

--- scripts/check-data.py
+++ scripts/check-data.py
@@ -18,7 +18,7 @@ def check_missing_metadata():
     for entry in published:
         related = set(entry['id_related'].split(',')) if entry['id_related'] else set()
         if related - published_ids:
-            print(f"Related videos f{entry['id_related']} for f{entry['id']} are unpublished")
+            print(f"Related videos {entry['id_related']} for {entry['id']} are unpublished")
             count += len(related)
         if len(entry['chords']) == 0 or len(entry['artists']) == 0 or len(entry['composers']) == 0:
             if "rabindra" in entry['album'].lower():
